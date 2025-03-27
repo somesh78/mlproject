5f782cde8a3a3e1e5d157182121cff5b290ac85d
somesh78 <rajputsomesh78@gmail.com>
2025-03-27 11:05:23 +0530

    left to complete

---
+++ src/components/model_trainer.py
@@ -0,0 +1,38 @@
+import os
+import sys
+from dataclasses import dataclass
+
+from catboost import CatBoostRegressor
+from sklearn.ensemble import AdaBoostRegressor, GradientBoostingRegressor, RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import save_object
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_filepath = os.path.join('artifactts', 'model.pkl')
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+
+    def initiate_model_trainer(self, train_array, test_array, preprocessor_path):
+        try:
+            logging.info("Split training and test input data")
+            X_train, y_train, X_test, y_test = (
+                train_array[:,:,-1],
+                train_array[:,:,-1],
+                test_array[:,:,-1],
+                test_array[:,:,-1]
+            )
+            models = {
+                "RandomForest": RandomForestRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                ""
+            }
